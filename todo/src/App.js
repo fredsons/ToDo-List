5de136ffdc6fddf1ced1165b22bbcc3f4ae82b0a
Fredson <85347233+fredsons@users.noreply.github.com>
2022-04-16 04:31:23 -0300

    adiciona funcao de deletar e editar todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -9,6 +9,26 @@ function App() {
   const [todos, setTodos] = useState([])
   const [loading, setLoading] = useState(false);
 
+  // Load todos on page load
+  
+  useEffect(() =>{
+
+    const loadData = async() => {
+      setLoading(true);
+      const res = await fetch(API + "/todos")
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((err) => console.log(err));
+
+      setLoading(false);
+      setTodos(res);
+    };
+
+    loadData()
+  }, []);
+  
+
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const todo = {
@@ -18,19 +38,49 @@ function App() {
       done: false,
     };
 
-
     await fetch(API + "/todos", {
       method: "POST",
       body: JSON.stringify(todo),
       headers: {
-        "Content - Type": "application/json",
+        "Content-Type": "application/json",
       },
     });
+
+    setTodos((prevState) => [...prevState, todo]);
 
     setTitle("");
     setTime("");
   };
 
+  const handleDelete = async (id) => {
+    await fetch(API + "/todos/" + id, {
+      method: "DELETE",
+    });
+
+    setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
+  };
+
+  
+  const handleEdit = async (todo) => {
+    todo.done = !todo.done;
+    const data =  await fetch(API + "/todos/" + todo.id, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setTodos((prevState) =>
+    prevState.map((t) => (t.id === data.id ? (t = data) : t))
+    )
+  };
+
+
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
 
 
   return (
@@ -67,6 +117,18 @@ function App() {
       <div className='list-todo'>
         <h2>Lista de tarefas:</h2>
         {todos.length === 0 && <p>Não há tarefas!</p>}
+        {todos.map((todo) =>(
+          <div className = "todo" key = {todo.id}>
+            <h3 className={todo.done ? "todo-done" : ""}>{todo.title}</h3>
+            <p>Duração: {todo.time}</p>
+            <div className="actions">
+              <span onClick={() => handleEdit(todo)}>
+                {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}
+              </span>
+              <BsTrash onClick={() => handleDelete(todo.id)} />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
